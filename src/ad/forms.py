efb6965370cf1b59bee17150177e44923ce85a60
phanthaikhang21127069 <101028729+phanthaikhang21127069@users.noreply.github.com>
2023-06-25 05:57:41 -0700

    dfdf

--- src/ad/forms.py
+++ src/ad/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 from . import models
 
 class SaveCategory(forms.ModelForm):
-    name = forms.CharField(max_length=250)
-    description = forms.Textarea()
-    status = forms.CharField(max_length=2)
+    # name = forms.CharField(max_length=250)
+    # description = forms.Textarea()
+    # status = forms.CharField(max_length=2)
 
     class Meta:
         model = models.Category
@@ -22,3 +22,20 @@ class UserFormRegister(UserCreationForm):
         #     'password1' : forms.CharField(attrs={'class' : 'form-control', 'id':'password1'}), 
         #     'password2' : forms.CharField(attrs={'class' : 'form-control', 'id':'password2'}), 
         # }
+
+class SaveSourceType(forms.ModelForm):
+    class Meta:
+        model = models.SourceType
+        fields = ('code', 'name', 'description',)
+
+
+class SaveLanguage(forms.ModelForm):
+    class Meta:
+        model = models.Language
+        fields = ('code','fullname',)
+        
+class SaveBook(forms.ModelForm):
+    class Meta:
+        model = models.Book
+        fields  = ('title', 'publication_year', 'author', 'category', 'description', 'sourcetype', 'language', 'image', 'quantity', 'status')
+
--- src/ad/models.py
+++ src/ad/models.py
@@ -12,3 +12,41 @@ class Category(models.Model):
 
     def __str__(self):
         return str(f'{self.name}')
+
+class SourceType(models.Model):
+    code = models.CharField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=250)
+    description = models.TextField(blank=True, null= True)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+
+    
+    def __str__(self):
+        return str(f'{self.name}')
+    
+class Language(models.Model):
+    code = models.CharField(primary_key=True, max_length=50)
+    fullname = models.CharField(max_length=250)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+
+    
+    def __str__(self):
+        return str(f'{self.fullname}')
+    
+class Book(models.Model):
+    title = models.CharField(primary_key=True, max_length=250)
+    publication_year = models.DateField()
+    author = models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null= True)
+    sourcetype = models.ForeignKey(SourceType, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    image = models.ImageField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=2, choices=(('1','Active'), ('2','Inactive')), default = 1)
+    delete_flag = models.IntegerField(default = 0)
+    date_added = models.DateTimeField(default = timezone.now)
+
+    def __str__(self):
+        return str(f'{self.title}')
