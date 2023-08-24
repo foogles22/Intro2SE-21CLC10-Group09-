3ba45c4770e73badb161ea89c0a71872eea97f8b
HieuTam <thtam21@clc.fitus.edu.vn>
2023-08-24 11:24:28 +0700

    Update views.py
    View for Authenticate

--- views.py
+++ views.py
@@ -27,3 +27,75 @@ def context_data():
         "system_name": "Wellib",
     }
     return context
+
+# AUTHENTICATE-AUTHENTICATE-AUTHENTICATE-AUTHENTICATE-AUTHENTICATE-AUTHENTICATE-AUTHENTICATE-AUTHENTICATE
+
+
+@user_is_authenticated
+def login_user(request):
+    context = context_data()
+    context["page_title"] = "Login"
+    if request.method == "POST":
+        auth = authenticate(
+            username=request.POST["userID"], password=request.POST["password"]
+        )
+        if auth is not None:
+            login(request, auth)
+            if request.user.profile.first_time == True:
+                messages.info(request, 'Please edit your profile!')
+                return redirect('edit_profile', request.user.id)
+            else:
+                messages.success(request, 'You have logged in!')
+                return redirect('home')
+        else:
+            messages.error(request, 'Login failed!')
+            return render(request, "authenticate/login.html", context)        
+    return render(request, "authenticate/login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
+
+@user_is_authenticated
+def register_user(request, *args, **kwargs):
+    context = context_data()
+    context["page_title"] = "Register"
+    if request.method == "POST":
+        register_form = forms.CustomUserCreationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Register successfully!')
+            return redirect('login')
+        else:
+            for error in register_form.errors.values():
+                messages.error(request, error)
+    return render(request, "authenticate/register.html", context)
+
+@login_required
+def edit_profile(request, id):
+    context = context_data()
+    context["page_title"] = "Edit Profile"
+    if request.method == "POST":
+        profile = models.Profile.objects.get(pk = id)
+        profile.first_time = False
+        profile.save()
+        profile = forms.EditProfile(request.POST, request.FILES , instance = profile)
+        if profile.is_valid():
+            profile.save()
+            messages.success(request, 'Edited profile successfully')
+        else:
+            for field in profile.errors.values():
+                for error in field:
+                    messages.error(request, error)
+    context["profile"] = models.Profile.objects.get(pk = id)
+    return render(request, 'ad/edit_profile.html', context)
+
+
+@login_required
+@allowed_users(allowed_roles=['ADMIN'])
+def delete_user(request, id):
+    user = User.objects.get(pk = id)
+    messages.success(request, 'Deleting acount succeed')
+    user.delete()
+    return redirect("user")
