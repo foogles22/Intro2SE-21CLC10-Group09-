1984b81e04ad58d72f33b3b91a15302eca98d3f1
HieuTam <thtam21@clc.fitus.edu.vn>
2023-08-24 11:17:21 +0700

    Update forms.py
    Add new fution for book store and circulation

--- forms.py
+++ forms.py
@@ -114,3 +114,74 @@ class SaveSourceType(forms.ModelForm):
             self.add_error('code',forms.ValidationError('This source type code already exists.'))
         except:
             return cleaned_data
+
+class SaveLanguage(forms.ModelForm):
+    code = forms.CharField(max_length=50)
+    fullname = forms.CharField(max_length=250)
+
+    class Meta:
+        model = models.Language
+        fields = ('code', 'fullname', )
+
+    def clean(self):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        code = self.data.get('code')
+        cleaned_data = self.cleaned_data
+        try:
+            if int(id) > 0:
+                models.Language.objects.exclude(id=id).get(code=code)
+            else:
+                models.Language.objects.get(code=code)
+            self.add_error('code',forms.ValidationError('This language code already exists.'))
+        except:
+            return cleaned_data
+
+class SaveBook(forms.ModelForm):
+    class Meta:
+        model = models.Book
+        fields = ('title', 'publication_year', 'author', 'category', 'description', 'sourcetype', 'language', 'image', 'quantity',)
+
+    def cleanTitle(self, title):
+        id = self.data['id'] if (self.data['id']).isnumeric() else 0
+        title = self.data.get('title')
+        if int(id) > 0 and title in models.Book.objects.exclude(id=id).values_list('title', flat=True):
+            self.add_error('title',forms.ValidationError('This book title already exists.'))
+            return False
+        elif int(id)==0 and title in models.Book.objects.values_list('title', flat=True):
+            self.add_error('title',forms.ValidationError('This book title already exists.'))
+            return False
+        else:
+            return True
+
+    def cleanYear(self, year):
+        if int(year) > timezone.now().year:
+            self.add_error('publication_year',forms.ValidationError('Publication year exceeds current year.'))
+            return False
+        else:
+            return True
+
+    def cleanQuantity(self, quantity):
+        if int(quantity) < 0:
+            self.add_error('quantity',forms.ValidationError('The book quantity is smaller than 0.'))
+            return False
+        else:
+            return True
+
+    def clean(self):
+        # print(self.data)
+        # print(cleaned_data)
+        
+        title = self.data.get('title')
+        self.cleanTitle(title)
+        
+        publication_year = self.data.get('publication_year')
+        self.cleanYear(publication_year)
+
+        quantity = self.data.get('quantity')
+        self.cleanQuantity(quantity)
+
+
+class SaveRequestBook(forms.ModelForm):
+    class Meta:
+        model = models.BookRequest
+        fields  = ('title', 'publication_year', 'author', 'category', 'description', 'sourcetype', 'language', 'image')
